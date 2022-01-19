536cd204b2ed19af3c1ccac194ee6f8d4bff456d
Ian Mercadal <imercadal@cifpfbmoll.eu>
2022-01-19 10:14:23 +0100

    feat: :art: Tabla de Contactos creada
    ER creado

---
+++ CRUD/database/migrations/2022_01_19_090640_create_contactos.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contactos', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('numero')->unique();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contactos');
+    }
+}
