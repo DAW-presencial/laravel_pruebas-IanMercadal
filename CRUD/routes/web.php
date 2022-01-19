798d04ab8c9937cddba49268c905e3052ca85d09
Ian Mercadal <imercadal@cifpfbmoll.eu>
2022-01-19 09:53:12 +0100

    feat: :art: Creando las rutas para CRUD
    Web.php con todo lo necesario

--- CRUD/routes/web.php
+++ CRUD/routes/web.php
@@ -2,23 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',HomeController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+// Contactos
+
+Route::group(['prefix' => '/contactos', 'middleware' => 'auth'],(function () {
+    Route::get('/',[ContactosController::class, 'index'])->name('contactos.index');
+    Route::get('/create',[ContactosController::class, 'create'])->name('contactos.create');
+    Route::post('/',[ContactosController::class, 'store'])->name('contactos.store');
+    Route::get('/{contacto}',[ContactosController::class, 'show'])->name('contactos.show');
+    Route::get('/{contacto}/edit',[ContactosController::class, 'edit'])->name('contactos.edit');
+    Route::put('/{contacto}',[ContactosController::class, 'update'])->name('contactos.update');
+    Route::delete('/{contacto}', [ContactosController::class,'destroy'])->name('contactos.destroy');
+}));
 
 require __DIR__.'/auth.php';
