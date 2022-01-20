75b7537d398819fe93e5b2bc9fe00eca73b61f36
Ian Mercadal <72464723+IanMercadal@users.noreply.github.com>
2022-01-20 09:56:40 +0100

    feat: :art: Generando rutas y resource
    Creando el controlador y modelo contacto

--- Agenda/routes/web.php
+++ Agenda/routes/web.php
@@ -1,17 +1,9 @@
 <?php
 
+use App\Http\Controllers\ContactoController;
+use App\Models\Contacto;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,5 +12,9 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+
+// Contactos
+Route::resource('contactos', ContactoController::class);
 
 require __DIR__.'/auth.php';
