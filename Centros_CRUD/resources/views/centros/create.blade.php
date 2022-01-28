16759704dbdf99748cff465b6f62a275dad40a9c
Ian Mercadal <72464723+IanMercadal@users.noreply.github.com>
2022-01-28 16:18:46 +0100

    feat: :art: 5. Required insertados
    El único campo no required es extraescolar

--- Centros_CRUD/resources/views/centros/create.blade.php
+++ Centros_CRUD/resources/views/centros/create.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 <h2 class="text-center">Crear Centro</h2>
 <div class="continer d-flex justify-content-center">
-    <form class="form border border-secondary text-start bg-light m-2 w-75" action="{{route('centros.store')}}" method="post">
+    <form class="form border border-secondary text-start bg-light m-2 w-75" action="{{route('centros.store', $centro)}}" method="post">
         @csrf
         @include('layouts.form')
         <button type="submit" class="btn btn-success p-1 m-2">Crear</button>
