01bf9693b5437bb5d976dce0cde3799812ba78d0
Ian Mercadal <imercadal@cifpfbmoll.eu>
2022-02-14 09:57:37 +0100

    feat: :art: Base - Mejorando rutas
    Mejor estructura de rutas

--- API_PRIMERA/routes/api.php
+++ API_PRIMERA/routes/api.php
@@ -15,12 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('products',ProductController::class);
-// Route::get('/products/search/{name}', [ProductController::class, 'search']);
+// Route::resource('products',ProductController::class);
 
-// Route::get('/products', [ProductController::class, 'index']);
-// Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
 
+// Rutas protegidas
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::get('/products/search/{name}', [ProductController::class, 'search']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 });
